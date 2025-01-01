c95f2f983a5c2baa74b56c5be1d9bcf15a96148f
Harshil IJ <marwahharshil@gmail.com>
2025-01-01 14:38:05 +0530

    SpringBootProject Setting up

---
+++ SpringBootTesting/src/main/java/com/learning/SpringBootTesting/SpringBootTestingApplication.java
@@ -0,0 +1,14 @@
+package com.learning.SpringBootTesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTestingApplication.class, args);
+		System.out.println("Hi");
+	}
+
+}
